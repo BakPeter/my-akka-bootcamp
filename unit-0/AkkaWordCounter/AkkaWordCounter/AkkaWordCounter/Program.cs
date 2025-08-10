a2a156c3a9366c117807e2a1d2107f35dde314b2
BakPeter <bak.pet@gmail.com>
2025-08-10 06:49:00 +0300

    WIP - unit 0

---
+++ unit-0/AkkaWordCounter/AkkaWordCounter/AkkaWordCounter/Program.cs
@@ -0,0 +1,21 @@
+﻿using Akka.Actor;
+using Akka.Event;
+using AkkaWordCounter;
+
+var myActorSystem = ActorSystem.Create("myActorSystem");
+myActorSystem.Log.Info("Hello from myActorSystem");
+
+// set Actor configurations
+var helloActorProps = Props.Create(() => new HelloActor());
+
+// create an instance of HelloActor and get IActorRef to it
+var helloActor = myActorSystem.ActorOf(helloActorProps, "helloActor");
+
+// send message to the HalloActor
+helloActor.Tell("Hello, my friend [tell]");
+
+// Use Ask to do request-response with helloActor
+var response = await helloActor.Ask<string>("Hello, my friend [ask]", TimeSpan.FromSeconds(3));
+Console.WriteLine("Response from HelloActor: {0}", response);
+
+await myActorSystem.Terminate();
