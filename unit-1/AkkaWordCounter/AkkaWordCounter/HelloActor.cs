8eb4a0a9bb9d738ba5292fa3628132ba2a94c50f
BakPeter <bak.pet@gmail.com>
2025-09-08 06:55:36 +0300

    WIP-unit1

---
+++ unit-1/AkkaWordCounter/AkkaWordCounter/HelloActor.cs
@@ -0,0 +1,16 @@
+﻿namespace AkkaWordCounter
+{
+    public class HelloActor : ReceiveActor
+    {
+        private readonly ILoggingAdapter _log = Context.GetLogger();
+        private int _helloCounter = 0;
+
+        public HelloActor()
+        {
+            Receive<string>(message =>
+            {
+                _log.Info("{0} {1}", message, _helloCounter++);
+            });
+        }
+    }
+}
---
+++ unit-1/AkkaWordCounter/AkkaWordCounter/Messages.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections.Immutable;
+using Google.Protobuf.WellKnownTypes;
+
+namespace AkkaWordCounter;
+
+public readonly record struct AbsoluteUri
+{
+    public AbsoluteUri(Uri value)
+    {
+        if (value.IsAbsoluteUri is false)
+        {
+            throw new ArgumentException("Value must be absolut", nameof(value));
+        }
+
+        Value = value;
+    }
+
+    public Uri Value { get; }
+
+    public override string ToString() => Value.ToString();
+}
+
+public interface IWithDocumentId
+{
+    public AbsoluteUri DocumentId { get; }
+}
+
+public static class DocumentCommands
+{
+    public sealed record ScanDocument(AbsoluteUri DocumentId) : IWithDocumentId;
+    public sealed record ScanDocuments(IReadOnlyList<AbsoluteUri> DocumentIds);
+}
+
+public static class DocumentEvents
+{
+    public sealed record DocumentScanFailed(AbsoluteUri DocumentId, string Reason) : IWithDocumentId;
+    public sealed record WordsFound(AbsoluteUri DocumentId, IReadOnlyList<string> Tokens) : IWithDocumentId;
+    public sealed record EndOfDocumentReached(AbsoluteUri DocumentId) : IWithDocumentId;
+    public sealed record CountsTabulatedForDocument(AbsoluteUri DocumentId, ImmutableDictionary<string, int> WordsFrequencies) : IWithDocumentId;
+    public sealed record CountsTabulatedForDocuments(IReadOnlyList<AbsoluteUri> DocumentIds, ImmutableDictionary<string, int> WordsFrequencies);
+}
+
+public static class DocumentQueries
+{
+    public sealed record FetchCounts(AbsoluteUri DocumentId) : IWithDocumentId;
+
+    public sealed class SubscribeToAllCounts
+    {
+        public static readonly SubscribeToAllCounts Instance = new();
+
+        private SubscribeToAllCounts() { }
+    }
+}
